977984f1870c3a6ae6bfcec76444874e04108466
Asuk Kono <asukiaaa@gmail.com>
2020-03-07 23:02:24 +0900

    Add source and example

---
+++ src/I2CMotors_asukiaaa.cpp
@@ -0,0 +1,76 @@
+#include <I2CMotors_asukiaaa.h>
+
+void I2CMotors_asukiaaa::setWire(TwoWire* wire) {
+  this->wire = wire;
+}
+
+void I2CMotors_asukiaaa::begin() {
+  if (this->wire == NULL) {
+    Wire.begin();
+    this->wire = &Wire;
+  }
+}
+
+int I2CMotors_asukiaaa::writeSpeed(int speedL, int speedR) {
+  uint8_t config = 0;
+  if (speedL < 0) {
+    speedL = - speedL;
+  } else {
+    config ^= 0b001;
+  }
+  if (speedR < 0) {
+    speedR = - speedR;
+  } else {
+    config ^= 0b010;
+  }
+  uint8_t l = speedL > 0xff ? 0xff : speedL;
+  uint8_t r = speedR > 0xff ? 0xff : speedR;
+  wire->beginTransmission(address);
+  wire->write(config);
+  wire->write(l);
+  wire->write(r);
+  return wire->endTransmission();
+}
+
+int I2CMotors_asukiaaa::readSpeed(int* speedL, int* speedR) {
+  wire->beginTransmission(address);
+  wire->write(I2C_MOTORS_ASUKIAAA_ADDRESS_CONFIG);
+  int result = wire->endTransmission();
+  if (result != 0) return result;
+  static const int buffLen = 3;
+  uint8_t buff[buffLen];
+  uint8_t buffIndex = 0;
+  wire->requestFrom(address, buffLen);
+  int receivedLen = 0;
+
+  while (wire->available() > 0) {
+    uint8_t d = wire->read();
+    if (buffIndex < buffLen) {
+      buff[buffIndex] = d;
+      ++buffIndex;
+    }
+    ++receivedLen;
+  }
+
+  if (receivedLen != buffLen) {
+    return I2C_MOTORS_ASUKIAAA_ERROR_READ_WRONG_LEN;
+  }
+
+  uint8_t config = buff[0];
+  int l = buff[1];
+  int r = buff[2];
+  if (config & 0b01 == 0) {
+    l = -l;
+  }
+  if (config & 0b10 == 0) {
+    r = -r;
+  }
+  *speedL = l;
+  *speedR = r;
+}
+
+int I2CMotors_asukiaaa::writeBrake() {
+  wire->beginTransmission(address);
+  wire->write(0b100);
+  return wire->endTransmission();
+}
---
+++ src/I2CMotors_asukiaaa.h
@@ -0,0 +1,38 @@
+#ifndef I2C_MOTORS_ASUKIAAA_H
+#define I2C_MOTORS_ASUKIAAA_H
+
+#include <Wire.h>
+
+#define I2C_MOTORS_ASUKIAAA_ADDRESS_CONFIG 0 // bit1: l direction, bit2: r direction, bit3: brake
+#define I2C_MOTORS_ASUKIAAA_ADDRESS_SPEED_L 1
+#define I2C_MOTORS_ASUKIAAA_ADDRESS_SPEED_R 2
+#define I2C_MOTORS_ASUKIAAA_MAX_SPEED 0xff
+
+#define I2C_MOTORS_ASUKIAAA_ERROR_READ_WRONG_LEN 5
+
+class I2CMotors_asukiaaa {
+ public:
+  I2CMotors_asukiaaa(uint8_t address) {
+    wire = NULL;
+    this->address = address;
+    reverseLeft = false;
+    reverseRight = false;
+  };
+
+  void setWire(TwoWire* wire);
+  void begin();
+  bool reverseLeft;
+  bool reverseRight;
+  int writeSpeed(int speedL, int speedR);
+  int readSpeed(int* speedL, int* speedR);
+  int writeBrake();
+
+ private:
+  TwoWire* wire;
+  uint8_t address;
+
+  static int i2cReadBytes(uint8_t address);
+  static int i2cWriteBytes();
+};
+
+#endif
